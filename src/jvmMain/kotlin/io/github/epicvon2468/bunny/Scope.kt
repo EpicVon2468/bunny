eb5d2e857a81d9a7222b3a04cab5353cc41642c8
EpicVon2468 <thomaschad0909@gmail.com>
2026-01-31 02:14:12 +1100

    Documentation for `Scope`, remove the `parent` property, prune aliases, add more TODOs... so many TODOs...

--- src/jvmMain/kotlin/io/github/epicvon2468/bunny/Scope.kt
+++ src/jvmMain/kotlin/io/github/epicvon2468/bunny/Scope.kt
@@ -4,24 +4,37 @@ import org.llvm.Core_h.*
 import org.llvm.Target_h.LLVMGetModuleDataLayout
 import org.llvm.Target_h.LLVMIntPtrTypeInContext
 
-// TODO: Rename to "Scope"
+/**
+ * A class to represent the values, types, and other info in a scope.
+ *
+ * Parsed functions and structs should be added to the type lookup with [childScope], storing the result into a global scope variable.
+ *
+ * By default, [Scope] only contains the built-in number types, size types, and pointer types.  See [globalScope] for the defaults.
+ *
+ * @author Mavity The Madity
+ */
 @ConsistentCopyVisibility
 data class Scope private constructor(
 	val typeLookup: Map<String, TypeInfo>,
 	// TODO: variable map for local & global variables :o
 	val functionLookup: Map<String, FunctionInfo> = emptyMap(),
-	val returnType: TypeInfo? = null,
-	val parent: Scope? = null
+	val returnType: TypeInfo? = null
 ) {
 
-	// TODO: copy parameters of a function (via LLVM) into new copies w/ alloca, then store in a map here (see also: MutableVariable, TypeInfo)
-
+	/**
+	 * Creates a new [Scope] with (potentially) updated values.
+	 *
+	 * @param addedTypes the new types to append to [typeLookup].
+	 * @param addedFunctions the new functions to append to [functionLookup].
+	 * @param returnType the new [TypeInfo] to represent the "return" type of the [Scope].  Use `null` to clear.
+	 * @return a new [Scope] with (potentially) update values.
+	 */
 	fun childScope(
 		addedTypes: Map<String, TypeInfo>? = null,
 		addedFunctions: Map<String, FunctionInfo>? = null,
-		returnType: TypeInfo? = this.returnType,
-		parent: Scope? = this
+		returnType: TypeInfo? = this.returnType
 	): Scope = Scope(
+		// TODO: this can overwrite builtin types.
 		this.typeLookup.let { lookup: Map<String, TypeInfo> ->
 			return@let if (addedTypes.isNullOrEmpty()) lookup
 			else lookup.toMutableMap().apply { putAll(addedTypes) }
@@ -30,23 +43,47 @@ data class Scope private constructor(
 			return@let if (addedFunctions.isNullOrEmpty()) lookup
 			else lookup.toMutableMap().apply { putAll(addedFunctions) }
 		},
-		returnType,
-		parent
+		returnType
 	)
 
+	/**
+	 * Merges the type and function lookups of [other] with this [Scope]'s lookups.
+	 *
+	 * @param other the [Scope] to append lookup entries from.
+	 * @return a new [Scope] with combined lookups between this and [other].
+	 */
 	fun mergeLookups(other: Scope): Scope = childScope(
 		other.typeLookup,
 		other.functionLookup
 	)
 
+	/**
+	 * Searches for a [type][TypeInfo] in the [type lookup][typeLookup], throwing [IllegalStateException] if the type cannot be found.
+	 *
+	 * @param name the name of the [type][TypeInfo] to search for.
+	 * @return a [type][TypeInfo] with the given [name].
+	 * @throws IllegalStateException if a [type][TypeInfo] of the given [name] cannot be found.
+	 */
 	fun lookupType(name: String): TypeInfo = lookupTypeOrNull(name) ?: error("No such key '$name' in type lookup!")
-	fun lookupTypeOrNull(name: String): TypeInfo? = typeLookup[name] ?: parent?.lookupTypeOrNull(name)
+	fun lookupTypeOrNull(name: String): TypeInfo? = typeLookup[name]
 
+	/**
+	 * Searches for a [function][FunctionInfo] in the [function lookup][functionLookup], throwing [IllegalStateException] if the function cannot be found.
+	 *
+	 * @param name the name of the [function][FunctionInfo] to search for.
+	 * @return a [function][FunctionInfo] with the given [name].
+	 * @throws IllegalStateException if a [function][FunctionInfo] of the given [name] cannot be found.
+	 */
 	fun lookupFunct(name: String): FunctionInfo = lookupFunctOrNull(name) ?: error("No such key '$name' in function lookup!")
-	fun lookupFunctOrNull(name: String): FunctionInfo? = functionLookup[name] ?: parent?.lookupFunctOrNull(name)
+	fun lookupFunctOrNull(name: String): FunctionInfo? = functionLookup[name]
 
 	companion object {
 
+		/**
+		 * Creates a new defaulted 'global' [Scope], using the [context][LLVMContextRef] and [module][LLVMModuleRef] provided.
+		 *
+		 * @return a new [Scope].
+		 */
 		// ONLY FOR TOP-LEVEL/ROOT ENV CREATION!!!
 		@JvmStatic
 		fun globalScope(context: LLVMContextRef, module: LLVMModuleRef) = Scope(
@@ -56,73 +93,23 @@ data class Scope private constructor(
 				}
 				fun MutableMap<String, TypeInfo>.put(value: TypeInfo) = put(*value.names.toTypedArray(), value = value)
 
-				put(TypeInfo(
-					LLVMIntPtrTypeInContext(context, LLVMGetModuleDataLayout(module)),
-					"isize", "usize",
-					"size_t",
-					"intptr_t", "uintptr_t"
-				))
-				put(TypeInfo(
-					LLVMVoidTypeInContext(context),
-					"", "void"
-				))
-				put(TypeInfo(
-					LLVMInt1TypeInContext(context),
-					"bool", "boolean",
-					"i1", "u8"
-				))
-				put(TypeInfo(
-					LLVMIntTypeInContext(context, 2),
-					"i2", "u2"
-				))
-				put(TypeInfo(
-					LLVMIntTypeInContext(context, 4),
-					"i4", "u4"
-				))
-				put(TypeInfo(
-					LLVMInt8TypeInContext(context),
-					"i8", "u8"
-				))
-				put(TypeInfo(
-					LLVMInt16TypeInContext(context),
-					"i16", "u16"
-				))
-				put(TypeInfo(
-					LLVMInt32TypeInContext(context),
-					"i32", "u32"
-				))
-				put(TypeInfo(
-					LLVMInt64TypeInContext(context),
-					"i64", "u64"
-				))
-				put(TypeInfo(
-					LLVMInt128TypeInContext(context),
-					"i128", "u128"
-				))
-				put(TypeInfo(
-					LLVMIntTypeInContext(context, 256),
-					"i256", "u256"
-				))
-				put(TypeInfo(
-					LLVMIntTypeInContext(context, 512),
-					"i512", "u512"
-				))
-				put(TypeInfo(
-					LLVMIntTypeInContext(context, 1024),
-					"i1024", "u1024"
-				))
-				put(TypeInfo(
-					LLVMFloatTypeInContext(context),
-					"f32", "float"
-				))
-				put(TypeInfo(
-					LLVMDoubleTypeInContext(context),
-					"f64", "double"
-				))
-				put(TypeInfo(
-					LLVMPointerTypeInContext(context, /*AddressSpace =*/ 0),
-					"ptr", "pointer"
-				))
+				// TODO: Type system needs a rework to handle signed vs unsigned...
+				put(TypeInfo(LLVMIntPtrTypeInContext(context, LLVMGetModuleDataLayout(module)), "size", "usize"))
+				put(TypeInfo(LLVMVoidTypeInContext(context), "", "void"))
+				put(TypeInfo(LLVMInt1TypeInContext(context), "bool"))
+				put(TypeInfo(LLVMIntTypeInContext(context, 2), "i2", "u2"))
+				put(TypeInfo(LLVMIntTypeInContext(context, 4), "i4", "u4"))
+				put(TypeInfo(LLVMInt8TypeInContext(context), "i8", "u8"))
+				put(TypeInfo(LLVMInt16TypeInContext(context), "i16", "u16"))
+				put(TypeInfo(LLVMInt32TypeInContext(context), "i32", "u32"))
+				put(TypeInfo(LLVMInt64TypeInContext(context), "i64", "u64"))
+				put(TypeInfo(LLVMInt128TypeInContext(context), "i128", "u128"))
+				put(TypeInfo(LLVMIntTypeInContext(context, 256), "i256", "u256"))
+				put(TypeInfo(LLVMIntTypeInContext(context, 512), "i512", "u512"))
+				put(TypeInfo(LLVMIntTypeInContext(context, 1024), "i1024", "u1024"))
+				put(TypeInfo(LLVMFloatTypeInContext(context), "f32"))
+				put(TypeInfo(LLVMDoubleTypeInContext(context), "f64"))
+				put(TypeInfo(LLVMPointerTypeInContext(context, 0), "ptr"))
 			}
 		)
 	}
