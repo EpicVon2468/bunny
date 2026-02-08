cfdf32dfd1fd097f58dd53c73afb0e5c4da438cf
EpicVon2468 <thomaschad0909@gmail.com>
2026-02-08 22:22:12 +1100

    Update Kotlin, update Gradle, disable native modules, explicitly configure :generated's JavaCompile tasks

--- generated/build.gradle.kts
+++ generated/build.gradle.kts
@@ -9,5 +9,12 @@ repositories {
 	mavenCentral()
 }
 
+tasks.withType<JavaCompile> {
+	options.apply {
+		encoding = "UTF-8"
+		isIncremental = true
+	}
+}
+
 // jextract --include-dir $DEV_LLVM_LOCATION/include/llvm-c --output . --target-package org.llvm --library LLVM-20 --use-system-load-library $DEV_LLVM_LOCATION/include/llvm-c/Core.h
 // jextract --include-dir $DEV_LLVM_LOCATION/include/llvm-c --output . --target-package org.llvm --library LLVM-20 --use-system-load-library $DEV_LLVM_LOCATION/include/llvm-c/Target.h
