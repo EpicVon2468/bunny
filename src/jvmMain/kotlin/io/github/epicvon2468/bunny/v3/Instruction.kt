baad8ec71525403271bde5a86ddd924735430fd2
EpicVon2468 <thomaschad0909@gmail.com>
2026-02-22 12:58:05 +1100

    add Instruction.Value

--- src/jvmMain/kotlin/io/github/epicvon2468/bunny/v3/Instruction.kt
+++ src/jvmMain/kotlin/io/github/epicvon2468/bunny/v3/Instruction.kt
@@ -1,24 +1,17 @@
 package io.github.epicvon2468.bunny.v3
 
-import io.github.epicvon2468.bunny.v3.Instruction.Expression
-import io.github.epicvon2468.bunny.v3.Instruction.Literal
-
 sealed interface Instruction {
 
 	data class FunctionBegin(val name: Identifier, val parameters: List<Type>) : Instruction
 	data class FunctionEnd(val name: Identifier) : Instruction
 
 	data class Define(val name: Identifier, val type: Type) : Instruction
-	data class Store(val name: Identifier, val value: Instruction /*= Literal | Expression*/) : Instruction {
-
-		init {
-			checkLitOrExpr(value)
-		}
-	}
+	data class Store(val name: Identifier, val value: Value) : Instruction
 	data class Load(val name: Identifier, val type: Type) : Instruction
 
-	data class Literal(val value: String, val type: Type) : Instruction
-	data class Expression(val name: Identifier, val returnType: Type) : Instruction
+	sealed interface Value : Instruction
+	data class Literal(val value: String, val type: Type) : Value
+	data class Expression(val name: Identifier, val returnType: Type) : Value
 
 	data class ExpressionBegin(val name: Identifier, val returnType: Type) : Instruction
 	data class ExpressionEnd(val name: Identifier) : Instruction
@@ -26,16 +19,7 @@ sealed interface Instruction {
 	data class GroupBegin(val returnType: Type) : Instruction
 	data class GroupEnd(val returnType: Type) : Instruction
 
-	data class Return(val value: Instruction /*= Literal | Expression*/) : Instruction {
-
-		init {
-			checkLitOrExpr(value)
-		}
-	}
-}
-
-fun checkLitOrExpr(value: Instruction) {
-	require(value is Literal || value is Expression) { "Value was not a Literal or an Expression!  Value: $value" }
+	data class Return(val value: Value) : Instruction
 }
 
 typealias Type = String
